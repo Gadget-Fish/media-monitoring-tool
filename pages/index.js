5d8a54575eb305ee0b3c3cb1b83881aee12bd37b
Gadget Fish <155038407+Gadget-Fish@users.noreply.github.com>
2025-07-10 15:13:13 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import MediaMonitoringTool from '../components/MediaMonitoringTool'
+
+export default function Home() {
+  return (
+    <div>
+      <head>
+        <title>生物医药舆情监测工具</title>
+      </head>
+      <MediaMonitoringTool />
+    </div>
+  )
+}
